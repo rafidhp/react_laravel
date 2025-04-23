e682f5535e7debe56e92470b0797efc0c42bb780
rafidhp <rafiislamipasha27@gmail.com>
2025-04-24 00:54:17 +0700

    feat: update vite conf

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,15 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    base: '/build/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.jsx'],
             refresh: true,
         }),
         react(),
-    ], server: {
+    ],
+    server: {
         https: true,
     }
 });
