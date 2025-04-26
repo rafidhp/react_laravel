fdfad66a21e80877a537c31de946d27200992058
rafidhp <rafiislamipasha27@gmail.com>
2025-04-26 23:08:02 +0700

    fix: fix vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     base: '/build/',
+    build: {
+        outDir: 'public/build',
+        manifest: true,
+        emptyOutDir: true,
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.jsx'],
