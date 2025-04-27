62dab18c312754cea40209970e1cb4604f0ce25b
rafidhp <rafiislamipasha27@gmail.com>
2025-04-27 22:56:29 +0700

    fix: try welcome code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>p</title>
-    @viteReactRefresh
-    @vite(['resources/js/app.jsx'])
+    {{-- @viteReactRefresh
+    @vite(['resources/js/app.jsx']) --}}
 </head>
 <body>
-    <div id="root"></div>
+    <div id="root">p</div>
 </body>
 </html>
